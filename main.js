7f3a66355f440d344dff17f708b9b11170d4c2eb
Amrit Atwal <amritatwal04@gmail.com>
2022-03-02 18:19:47 +0000

    Refactored code

--- main.js
+++ main.js
@@ -1,127 +1,94 @@
-let targetWord;
-let savedData;
-let wordFromData;
-let definitions = []
 let subHeading;
-let phonetic;
 let mainList;
-let phoneticText;
 let container = document.querySelector("ol");
-
-async function getWord() {
-    const dictionaryAPI = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${targetWord}`)
-    if (dictionaryAPI.ok) {
-        const data = await dictionaryAPI.json();
-        savedData = data;
-        console.log(savedData)
-        document.querySelector(".main").style.visibility = "visible";
-        findDefinitions();
-        populateList();
-        addPhonetic()
-        // createSubHeading();
-    } else {
-        // Catch the error for missing word 
-        document.querySelector(".main").style.visibility = "visible";
-        handleWordError()
-    }
-};
-
-
-// Function to locate word's definitions 
-
-function findDefinitions() {
-    for (let index = 0; index < savedData.length; index++) {
-        wordFromData = ((savedData[index]));
-        definitions = (wordFromData["meanings"][0]["definitions"]);
-    }
-};
-
+let phoneticElement = document.querySelector(".results--phonetic");
 
 // making the text entry box listen for the enter key, which puts entered work into targetWord variable
 // variable to target the word entry box
 const inputBox = document.getElementById("userInput");
 
+async function getWord() {
+    // The target word is appended to the URL in order to recieve data for the specific word inputted by the user 
+    const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${targetWord}`);
+    // Handle the error if there is an incorrect word or response !== 200
+    if (!data.ok) {
+        document.querySelector(".main").style.visibility = "visible";
+        handleWordError()
+    }
+    const response = await data.json();
+    document.querySelector(".main").style.visibility = "visible";
+    const phonetic = response[0].phonetic;
+    const definitions = findDefinitions(response);
+    console.log(definitions)
+    populateContent(phonetic, definitions);
+};
 
-// Select 
+// Locate and store the word's definitions into an initialised empty array
+function findDefinitions(data) {
+    let listOfdefinitions = [];
+    for (let index = 0; index < data.length; index++) {
+        listOfdefinitions.push(data[0].meanings[0].definitions[index].definition);
+    }
+    return listOfdefinitions;
+};
 
+
+// Select the necessary elements whose inner text will be updated with the inputted word
 const h3 = document.querySelector("h3");
-// Select p tag with class "word" to update with word 
 const p = document.querySelector(".word");
 
+
+// Amend the inner text of h3 and p to our inputted word oncen the enter key pressed (TODO: or search icon is clicked)
+// Once the word is retrieved invoke getWord() 
+// The async function will only be invoked if there is a word to fetch data for
+inputBox.addEventListener("keyup", handleEnterKey);
+
 function handleEnterKey(e) {
-    // Read the value from the value property
     let word = e.target.value;
     if (e.keyCode === 13) {
         h3.innerText = word;
         p.innerText = word;
-    }
-};
-
-
-// Change h3 to targetWord if enter key pressed or search icon is clicked
-inputBox.addEventListener("keyup", handleEnterKey);
-
-// function for event
-function receiveUserWord(event) {
-    // take inlineText from the text entry box, and put it in the variable targetWord
-    if (event.keyCode === 13) {
-        targetWord = event.target.value
-        console.log(targetWord);
+        targetWord = e.target.value
         getWord();
     }
 };
 
-inputBox.addEventListener("keyup", receiveUserWord);
-
-
-// Pull through definition info from savedData
-
-// definitions variable contains an array of definitions for the word retrieved
-// take the definitions from this array, and place them into individual list items
-
-function populateList() {
+// Pull through definition info
+// The definitions variable contains an array of definitions for the word retrieved
+// Take the definitions from this array, and place them into individual list items
+function populateContent(word, definitions) {
     clearContents()
-    //first, create list items
+    addPhonetic(word);
     for (let i = 0; i < definitions.length; i++) {
         let listItem = document.createElement("li");
         let list = document.querySelector("ol");
         list.appendChild(listItem);    // append list items
-        listItem.innerText = definitions[i]["definition"]  //add information to the items
-        console.log(definitions[i]["definition"])
-        console.log(definitions)
-        console.log(listItem)
+        listItem.innerText = definitions[i]  //add information to the items
     }
 };
 
-
-function handleWordError() {
-    clearContents()
-    h3.innerText = `I don't think the word exists!`;
+// Retrieve phonetic from data and add to inner text of a p tag
+function addPhonetic(phonetic) {
+    if (phonetic === undefined) {
+        phoneticElement.innerText = "Apologies, no phonetic found.";
+    } else {
+        phoneticElement.innerText = phonetic;
+    }
 }
 
+// Clear the currently displayed list of its text, list items, header and phonetic
 function clearContents() {
-    // Clear list
-    let oldList = document.querySelectorAll("ol > p, li, h2, .phonetic");
+    let oldList = document.querySelectorAll("ol > p, li, h2");
     for (let index = 0; index < oldList.length; index++) {
         oldList[index].remove();
     }
+    phoneticElement.innerText = "";
 };
 
-// use insertBefore to place an h2 that contains the current word being defined
-// function createSubHeading() {
-//     subHeading = document.createElement("h2");
-//     mainList = document.querySelector("ol");
-//     let container = document.getElementById("container");
-//     subHeading.innerText = `${targetWord}:`;
-//     container.insertBefore(subHeading, mainList);
-// }
-
-function addPhonetic() {
-    phonetic = document.querySelector(".results--phonetic");
-    phoneticText = wordFromData["phonetic"];
-    if (phoneticText === undefined) {
-        phonetic.innerText = "Apologies, no phonetic found.";
-    } else {
-        phonetic.innerText = phoneticText;
-    }
+// Clear the contents of the data retrieved from the previous fetch request and display a error message to the user 
+function handleWordError() {
+    clearContents()
+    h3.innerText = `I don't think the word exists!`;
+    let microphoneIcon = document.querySelector(".material-icons");
+    microphoneIcon.display = "hide";
 }
